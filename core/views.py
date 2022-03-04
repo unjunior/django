dc3cc9bf2d2e86112166d08d2ff8ac779dcff82f
Ubirajara Junior <ununesjunior@gmail.com>
2022-03-04 08:22:30 -0300

    Apresentando dados do banco de dados no template

---
+++ core/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+
+from  .models import Produto
+
+
+def index(request):
+    produtos = Produto.objects.all()
+
+
+
+    context = {
+        'curso': 'Programação web com Django Framework',
+        'outro': 'Djando é legal',
+        'produtos': produtos
+    }
+
+    return render(request, 'index.html', context)
+
+
+def contato(request):
+    return render(request, 'contato.html')
+
+def produto(request, pk):
+    prod = Produto.objects.get(id=pk)
+
+    context = {
+        'produto': prod
+    }
+    return render(request, 'produto.html', context)
